c644b26bc7e80046b895fb37cf798efb8dfad0f3
hossainmamun <hossainmamun337@gmailcom>
2022-11-19 13:29:17 +0600

    fix some issue

--- src/Components/HomeComponent/Banner.jsx
+++ src/Components/HomeComponent/Banner.jsx
@@ -26,35 +26,45 @@ const Banner = () => {
    }, []);
 
    return (
-      <div className='lg:flex lg:justify-between items-center text-center min-h-screen mt-[150px] lg:mt-0'>
-         <div className='space-y-6 px-10 lg:px-0 w-full h-[50%] lg:h-full lg:w-[45%] text-center lg:text-start lg:pl-16 xl:pl-24 2xl:pl-36'>
-            <div className='space-y-2'>
-               <h3 className='capitalize font-bold text-base  md:text-lg'>
-                  {banner_title}
+      <div className='flex justify-center items-center min-h-screen mt-[150px] lg:mt-0'>
+         {banner.length === 0 ? (
+            <div className='flex justify-center items-center min-h-screen'>
+               <h3 className='font-bold text-base sm:text-lg md:text-xl lg:text-2xl'>
+                  please wait...
                </h3>
-               <span className='font-extrabold capitalize text-xl md:text-3xl lg:text-4xl text-[#3a0ca3]'>
-                  <Typewriter
-                     options={{
-                        strings: typewriterData,
-                        autoStart: true,
-                        loop: true,
-                     }}
-                  />
-               </span>
             </div>
-            <p>{banner_detail}</p>
-            <div>
-               <Link to='/book-appointment'>
-                  <button className='font-base font-semibold border-2 border-[#3a0ca3] bg-[#3a0ca3] text-white px-10 py-3 rounded-lg hover:bg-transparent hover:text-[#3a0ca3] duration-150'>
-                     BOOK AN APPOINTMENT
-                  </button>
-               </Link>
-            </div>
-         </div>
+         ) : (
+            <div className='lg:flex lg:justify-between items-center text-center min-h-screen mt-[150px] lg:mt-0'>
+               <div className='space-y-6 px-10 lg:px-0 w-full h-[50%] lg:h-full lg:w-[45%] text-center lg:text-start lg:pl-16 xl:pl-24 2xl:pl-36'>
+                  <div className='space-y-2'>
+                     <h3 className='capitalize font-bold text-base  md:text-lg'>
+                        {banner_title}
+                     </h3>
+                     <span className='font-extrabold capitalize text-xl md:text-3xl lg:text-4xl text-[#3a0ca3]'>
+                        <Typewriter
+                           options={{
+                              strings: typewriterData,
+                              autoStart: true,
+                              loop: true,
+                           }}
+                        />
+                     </span>
+                  </div>
+                  <p>{banner_detail}</p>
+                  <div>
+                     <Link to='/book-appointment'>
+                        <button className='font-base font-semibold border-2 border-[#3a0ca3] bg-[#3a0ca3] text-white px-10 py-3 rounded-lg hover:bg-transparent hover:text-[#3a0ca3] duration-150'>
+                           BOOK AN APPOINTMENT
+                        </button>
+                     </Link>
+                  </div>
+               </div>
 
-         <div className='w-full lg:w-[45%] pt-10 sm:pt-10 lg:pt-0 px-10 lg:px-0'>
-            <img src={imgUrl} className='w-full' alt='' />
-         </div>
+               <div className='w-full lg:w-[45%] pt-10 sm:pt-10 lg:pt-0 px-10 lg:px-0'>
+                  <img src={imgUrl} className='w-full' alt='' />
+               </div>
+            </div>
+         )}
       </div>
    );
 };
--- src/Pages/ProfessionalList.jsx
+++ src/Pages/ProfessionalList.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const ProfessionalList = () => {
-   const [banner, setBanner] = useState([]);
+   const [professional, setProfessional] = useState([]);
 
    const fetchProfessionals = async () => {
       try {
@@ -11,7 +11,7 @@ const ProfessionalList = () => {
             'https://doctor-appointment-api.onrender.com/api/professional'
          );
          if (response.status === 200) {
-            setBanner(response.data);
+            setProfessional(response.data);
          }
       } catch (error) {
          console.log(error);
@@ -22,52 +22,62 @@ const ProfessionalList = () => {
       fetchProfessionals();
    }, []);
    return (
-      <div className='mt-[150px] min-h-screen space-y-10 mx-10 lg:mx-32 xl:mx-40 2xl:mx-60'>
-         <div className='text-center'>
-            <h3 className='capitalize font-extrabold text-lg md:text-xl'>
-               our all professionals
-            </h3>
-         </div>
+      <div className='flex justify-center items-center min-h-screen'>
+         {professional.length === 0 ? (
+            <div className='flex justify-center items-center'>
+               <h3 className='font-bold text-base sm:text-lg md:text-xl lg:text-2xl'>
+                  Wait... professionals is loading
+               </h3>
+            </div>
+         ) : (
+            <div className='mt-[150px] min-h-screen space-y-10 mx-10 lg:mx-32 xl:mx-40 2xl:mx-60'>
+               <div className='text-center'>
+                  <h3 className='capitalize font-extrabold text-lg md:text-xl'>
+                     our all professionals
+                  </h3>
+               </div>
 
-         <div className='grid sm:grid-cols-2 gap-10 w-full'>
-            {banner.map((professional, index) => {
-               const {
-                  imgUrl,
-                  professional_name,
-                  professional_designation,
-                  professional_degree,
-               } = professional;
-               return (
-                  <div
-                     className='flex flex-col xl:flex-row rounded-lg bg-white shadow-lg'
-                     key={index}>
-                     <img
-                        className='w-full md:h-auto object-fill xl:w-48 2xl:w-60 rounded-t-lg xl:rounded-l-lg xl:rounded-r-none'
-                        src={imgUrl}
-                        alt=''
-                     />
-                     <div className='p-6 flex flex-col justify-start space-y-2'>
-                        <h5 className='text-gray-900 text-xl font-medium capitalize'>
-                           {professional_name}
-                        </h5>
-                        <p className='text-gray-700 text-base'>
-                           Role: {professional_designation}
-                        </p>
-                        <p className='text-gray-600 text-base'>
-                           Degree: {professional_degree}
-                        </p>
-                        <div>
-                           <Link to='/book-appointment'>
-                              <button className='font-base border-2 border-[#3a0ca3] bg-[#3a0ca3] text-white px-4 py-1 rounded-lg hover:bg-transparent hover:text-[#3a0ca3] duration-150'>
-                                 APPOINTMENT
-                              </button>
-                           </Link>
+               <div className='grid sm:grid-cols-2 gap-10 w-full'>
+                  {professional.map((professional, index) => {
+                     const {
+                        imgUrl,
+                        professional_name,
+                        professional_designation,
+                        professional_degree,
+                     } = professional;
+                     return (
+                        <div
+                           className='flex flex-col xl:flex-row rounded-lg bg-white shadow-lg'
+                           key={index}>
+                           <img
+                              className='w-full md:h-auto object-fill xl:w-48 2xl:w-60 rounded-t-lg xl:rounded-l-lg xl:rounded-r-none'
+                              src={imgUrl}
+                              alt=''
+                           />
+                           <div className='p-6 flex flex-col justify-start space-y-2'>
+                              <h5 className='text-gray-900 text-xl font-medium capitalize'>
+                                 {professional_name}
+                              </h5>
+                              <p className='text-gray-700 text-base'>
+                                 Role: {professional_designation}
+                              </p>
+                              <p className='text-gray-600 text-base'>
+                                 Degree: {professional_degree}
+                              </p>
+                              <div>
+                                 <Link to='/book-appointment'>
+                                    <button className='font-base border-2 border-[#3a0ca3] bg-[#3a0ca3] text-white px-4 py-1 rounded-lg hover:bg-transparent hover:text-[#3a0ca3] duration-150'>
+                                       APPOINTMENT
+                                    </button>
+                                 </Link>
+                              </div>
+                           </div>
                         </div>
-                     </div>
-                  </div>
-               );
-            })}
-         </div>
+                     );
+                  })}
+               </div>
+            </div>
+         )}
       </div>
    );
 };
